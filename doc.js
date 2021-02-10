7c9197586089765d28cf48d8887f97eb1d30476f
mariajosediazdelgado <diaz.maria@correo.unimet.edu.ve>
2021-02-09 21:56:19 -0400

    intento2 recontraintentado

--- doc.js
+++ doc.js
@@ -1,27 +1,43 @@
 
-window.addEventListener('load', function(){
-    
-    console.log('el contenido ha cargado')
-    
-    var imagenes = []
-    var tiempo = 1000
+var slideIndex = 0;
+showSlides();
 
-    imagenes[0]='slider/imagen1.jpg'
-    imagenes[1]='slider/imagen2.jpg'
-    imagenes[2]='slider/imagen3.jpg'
-
-    function cambiar(){
-
-        var i = 0
-        document.slider.src = imagenes[2]
-
-        if(i<2){
-            i++;
-        }else{
-            i=0
-        }
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
     }
-    window.setInterval(cambiar(), tiempo)
-})
+    slideIndex++;
+    
+    if(slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides,2000);
+}
 
+
+/*let img = document.querySelector("img"),
+    slide = document.querySelector("slide"),
+    imagenes = ["slider/imagen1.jpg","slider/imagen1.jpg","slider/imagen1.jpg"],
+    i=0;
+
+slide.addEventListener("click",function(){
+    img.setAttribute('src', imagenes[i]);
+    i++;
+});
+slide; */
+
+/*const imagen = document.getElementById("slider");
+
+let i=0
+
+setInterval(function(){
+    imagen.src=`slider/${i}.jpg`;
+    
+    if(i < 3){
+        i++;
+    }else{
+        i=0;
+    }
+},2000)*/
 
